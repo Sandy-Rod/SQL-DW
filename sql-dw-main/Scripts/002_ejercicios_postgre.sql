6a8b9d9e411b4d26cbac87963775c7b7a4236c53
Sandy Rodriguez <sandy.rodriguezaponte@hotmail.com>
2024-09-09 20:24:17 +0200

    main

---
+++ sql-dw-main/Scripts/002_ejercicios_postgre.sql
@@ -0,0 +1,80 @@
+
+-- Recupera todos los clientes (name y email) ordenados alfabéticamente por nombre.
+
+
+
+-- Encuentra el número total de reservas.
+
+
+
+-- Muestra los detalles de las reservas (reservation_id, start_date, end_date) para un cliente específico (puedes elegir el customer_id).
+
+
+
+-- Encuentra el nombre y apellido de los propietarios que tienen apartamentos.
+
+
+
+-- Recupera el nombre y la ubicación de los apartamentos propiedad de un propietario específico (puedes elegir el owner_id).
+
+
+
+-- Muestra las comodidades asociadas a un apartamento específico (puedes elegir el apartment_id).
+
+
+
+-- Encuentra los clientes que no han realizado ninguna reserva.
+
+
+
+-- Obtén el número total de apartamentos en una ubicación específica (puedes elegir la ubicación).
+
+
+
+-- Recupera la información completa de las reservas (reservation_id, start_date, end_date) para los apartamentos que tienen un precio superior a cierto valor (puedes elegir el valor).
+
+
+
+-- Encuentra el propietario con más apartamentos y muestra sus detalles (name, surname, email).
+
+
+
+-- Muestra la ubicación y el número de reservas para cada ubicación, ordenado por número de reservas descendente.
+
+
+
+-- Encuentra los clientes que han realizado al menos una reserva y muestra cuántas reservas han hecho.
+
+
+
+-- Obtén el precio promedio de los apartamentos.
+
+
+
+-- Recupera el nombre de las comodidades que están asociadas a más de un apartamento.
+
+
+
+-- Encuentra los propietarios que tienen apartamentos en más de una ubicación.
+
+
+
+-- Muestra las reservas que están programadas desde una fecha futura (puedes elegir la fecha).
+
+
+
+-- Obtén el nombre de los clientes que han realizado reservas en al menos dos ubicaciones diferentes.
+
+
+
+-- Obtén la ubicación con el mayor número de comodidades, mostrando el nombre de la ubicación y la cantidad de comodidades.
+
+
+
+-- Obtén la cantidad total de reservas realizadas por cada cliente, ordenado de mayor a menor.
+
+
+
+-- Clientes que han repetido en un mismo apartment
+
+
